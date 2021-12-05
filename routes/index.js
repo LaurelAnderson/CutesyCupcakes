3a5198f5a3184cff56150023a01c9e7f6d34107f
Irina Bejan <irina.bejan@wsu.edu>
2021-12-05 03:03:07 -0800

    Add session store and cart model

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const { countDocuments } = require('../models/product');
 var router = express.Router();
+var Cart = require('../models/cart');
 
 var Product = require('../models/product');
 
@@ -8,6 +9,21 @@ var Product = require('../models/product');
 router.get('/', function(req, res, next) {
   Product.find(function(err, docs) {
     res.render('shop/index', { title: 'Cutesy', products: docs });
+  });
+});
+
+router.get('/add-to-cart/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  Product.findById(productId, function(err, product) {
+    if (err) {
+      return res.redirect('/');
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect('/');
   });
 });
 
