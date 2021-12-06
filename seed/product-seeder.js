a77075ca121b671cc00d72fdadf7781bf2163c44
LaurelAnderson <laureloanderson@gmail.com>
2021-12-05 20:26:16 -0800

    update pictures of all products

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -6,79 +6,77 @@ mongoose.connect('mongodb://localhost:27017/cupcakes');
 
 var products = [
     new Product({
-        imagePath: 'https://cdn.shopify.com/s/files/1/0266/2304/9805/products/RedVelvet_Shopify_1200x1200.png?v=1599664692',
+        imagePath: 'https://sweetfreedombakeshop.com/wp-content/uploads/IMG_2845.jpg',
         title: 'Red Velvet',
         description: 'Moist cocoa-y cake with a luscious cream cheese frosting',
         price: 3.25
     }),
     new Product({
-        imagePath: 'https://www.pngall.com/wp-content/uploads/5/Yummy-Cupcake-PNG-Picture.png',
+        imagePath: 'https://sweetfreedombakeshop.com/wp-content/uploads/Dreamsicle-Cupcake.png',
         title: 'Fun in the Sun',
-        description: 'Tangy lemon cake with strawberry lemonade frosting. Now this is a party!',
+        description: 'Tangy orange cake with mimosa frosting. Now this is a party!',
         price: 3.25
     }),
     new Product({
-        imagePath: 'https://r1.hiclipart.com/path/884/542/353/97e13r60numghp0d44s8o5v7nf-00ac2da49e4a9bedaa31b37008abc192.png?dl=1',
-        title: 'London Mudd',
-        description: 'Dark chocolate cake with a luscious earl grey frosting. Jolly good!',
+        imagePath: 'https://sweetfreedombakeshop.com/wp-content/uploads/Lavender-Earl-Gray-Cupcake.png',
+        title: 'London Smog',
+        description: 'Lavender earl grey cake with a luscious earl grey frosting. Jolly good!',
         price: 3.75
     }),
     new Product({
-        imagePath: 'https://www.pngall.com/wp-content/uploads/5/Yummy-Cupcake-PNG.png',
+        imagePath: 'https://sweetfreedombakeshop.com/wp-content/uploads/White-Velvet-Cupcake.jpg',
         title: 'Not-So-Plain Vanilla',
         description: 'Moist vanilla cake topped with vanilla bourbon frosting. Anything but plain!',
         price: 3.25
     }),
     new Product({
-        imagePath: 'https://www.pngkit.com/png/full/427-4273444_delicious-strawberry-cupcakes-cupcake-with-strawberry-png.png',
+        imagePath: 'https://sweetfreedombakeshop.com/wp-content/uploads/Strawberry-Lemonade-Cupcake.jpg',
         title: 'Strawberry Not-So-Shortcake',
         description: 'A shortcake base with a tall helping of strawberry sweet cream frosting.',
         price: 4.00
     }),
     new Product({
-        imagePath: 'https://www.pngall.com/wp-content/uploads/5/Cupcake-Dessert-PNG-Free-Image.png',
-        title: 'Magical Unicorn',
-        description: 'Rainbow vanilla cake with colorful vanilla frosting. Great for children’s birthday parties.',
+        imagePath: 'https://sweetfreedombakeshop.com/wp-content/uploads/IMG_2281-copy.jpg',
+        title: 'Snickerdoo-You-Want-Some',
+        description: 'Snickerdoodle cake with delectable cinnamon frosting. Yes I do want some!',
         price: 3.25
     }),
     new Product({
-        imagePath: 'https://www.pngall.com/wp-content/uploads/5/Cupcake-PNG-Clipart.png',
-        title: 'Rocky Road Redemption',
-        description: 'Epic chocolate cake, milk chocolate frosting, covered in toasted peanuts, marshmallows, and chocolate fudge. ',
-        price: 4.00
-    }),
-    new Product({
-        imagePath: 'https://www.pngall.com/wp-content/uploads/5/Cupcake-PNG-Image-File.png',
-        title: 'Drink the Koolaid',
-        description: 'Vanilla cake with a blue raspberry Koolaid frosting. Oh-Yeah!',
-        price: 3.75
-    }),
-    new Product({
-        imagePath: 'https://www.pngmart.com/files/5/Cupcake-PNG-Photos.png',
+        imagePath: 'https://sweetfreedombakeshop.com/wp-content/uploads/IMG_2977-1.jpg',
         title: 'Peanut Butter Cup-Cake',
         description: 'Creamy peanut butter frosting with milk chocolate cake. Name a more iconic duo.',
         price: 3.75
     }),
     new Product({
-        imagePath: 'https://purepng.com/public/uploads/large/chocolate-flower-cupcake-hzo.png',
+        imagePath: 'https://sweetfreedombakeshop.com/wp-content/uploads/IMG_2438.jpg',
         title: 'Devil’s in the Cupcake',
         description: 'Devil’s food cake with tempting fudge frosting. Wicked smart!',
         price: 3.00
     }),
     new Product({
-        imagePath: 'https://www.pikpng.com/pngl/b/58-581627_cupcake-transparent-background-cupcake-png-clipart.png',
-        title: 'He Loves Me, He Loves Me Not – Oh, Cupcake!',
-        description: 'Strawberry frosting, decadent chocolate cake, chocolate sprinkles, and romantic thoughts.',
+        imagePath: 'https://sweetfreedombakeshop.com/wp-content/uploads/IMG_3397.jpg',
+        title: 'He Loves Me, He Loves Me Not',
+        description: 'Strawberry frosting, decadent chocolate cake, and romantic thoughts.',
         price: 3.75
     }),
     new Product({
-        imagePath: 'https://p7.hiclipart.com/preview/308/4/971/cupcake-frosting-icing-dessert-candy-vanilla.jpg',
+        imagePath: 'https://sweetfreedombakeshop.com/wp-content/uploads/Pink-Champagne-Cupcake.jpg',
         title: 'Pretty in Pink',
         description: 'Pink velvet cake with smooth cream cheese frosting. A perfect pink pleasure!',
         price: 4.00
     }),
-
-
+    new Product({
+        imagePath: 'https://sweetfreedombakeshop.com/wp-content/uploads/Thin-Mint-Cupcake.jpg',
+        title: 'Chocolate Mint Madness',
+        description: 'Rich chocolate cake with crisp mint frosting. Perfect for St. Patrick’s Day!',
+        price: 3.25
+    }),
+    new Product({
+        imagePath: 'https://sweetfreedombakeshop.com/wp-content/uploads/Funfetti-Cupcake-Natural.png',
+        title: 'Finest Funfetti',
+        description: 'Vanilla cake and vanilla frosting bursting with colorful jimmies.',
+        price: 3.75
+    }),
 
 ];
 
